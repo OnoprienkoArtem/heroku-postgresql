3ca8bd05ff6c8f1711f2266eb15ccbe3ffaee930
OnoprienkoArtem <o.artem.e@gmail.com>
2021-04-26 23:51:53 +0300

    resolve comments

--- config/config.ts
+++ config/config.ts
@@ -1,11 +1,11 @@
-import {Options} from "sequelize/types";
+import { Options } from 'sequelize';
 
 const config: {development: Options} = {
     development: {
-        username: 'ouaicwafadjnvv',
-        password: 'f066d15defe2629afdc4f8a614e67cb6e4c468ede2b7f0df4adca76d9469d9fb',
-        database: 'da1b2a71vi6jrs',
-        host: 'ec2-54-228-99-58.eu-west-1.compute.amazonaws.com',
+        username: '',
+        password: '',
+        database: '',
+        host: '',
         dialect: 'postgres',
         dialectOptions: {
             ssl: {
