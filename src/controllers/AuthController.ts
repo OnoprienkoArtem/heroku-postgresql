4f3151513ddf1ac098f372d7cba19a18db8a687e
OnoprienkoArtem <o.artem.e@gmail.com>
2021-05-29 01:01:40 +0300

    work with jwt

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -3,44 +3,45 @@ import { Op } from 'sequelize';
 import User from '../model/user';
 import jwt from 'jsonwebtoken';
 import AuthService from '../services/Auth.service';
+import config from '../../config/config.json';
+import Api404Error from '../utils/handleError/handleError';
 
 export default class AuthController {
     constructor(public readonly authService: AuthService) {
     }
 
     public login = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-        console.log(req.body);
-
-        const candidate = await User.findOne({
-            where: {
-                login: {
-                    [Op.like]: `%${req.body.username}%`
-                },
-                password: {
-                    [Op.like]: `%${req.body.password}%`
-                }
-            }
-        });
-
-        if (candidate) {
-            const token = jwt.sign({
-                email: candidate.email,
-                userId: candidate._id
-            }, keys.jwt, {expiresIn: 3600});
-
-            res.status(200).json({
-                token: `Bearer ${token}`
-            });
-        } else {
-            res.status(401).json({
-                message: 'Passwords mismatch. Try again.'
-            });
-        }
-
         try {
+            console.log(req.body);
+
+            const candidate = await User.findOne({
+                where: {
+                    login: req.body.username,
+                    password: req.body.password
+                },
+            });
+
+            // throw new Api404Error(`User with not found.`);
+
+        // if (candidate) {
+        //     const token = jwt.sign({
+        //         login: candidate?.get('login'),
+        //         id: candidate?.get('id')
+        //     }, "jwt-secret", {expiresIn: 3600});
+        //
+        //     res.status(200).json({
+        //         token: `Bearer ${token}`
+        //     });
+        // } else {
+        //     res.status(401).json({
+        //         message: 'Passwords mismatch. Try again.'
+        //     });
+        // }
+
+
 
         } catch (error) {
-
+            return next(error);
         }
     }
 }
